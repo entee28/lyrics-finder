d66591c3f96240643ace2be9e4deb208ea26472c
Thanh Nguyen <puffle.nnt@gmail.com>
2022-01-09 10:45:22 +0700

    added dispatch and reducer

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -6,12 +6,18 @@ import searchIcon from "../../res/search-solid.svg"
 const Search = () => {
     const [trackTitle, setTrackTitle] = useState('');
 
-    const findTrack = (e) => {
+
+    const findTrack = (dispatch, e) => {
         e.preventDefault();
 
         axios.get(`http://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q=${trackTitle}&page_size=3&page=1&s_artist_rating=desc&s_track_rating=desc&apikey=9de0259a0b660a3ce82f1c903e01fc64`)
         .then(res => {
-            console.log(res.data);
+            dispatch({
+                type: 'SEARCH_TRACKS',
+                payload: res.data.message.body.track_list,
+            });
+
+            setTrackTitle('');
         })
         .catch(err => console.log(err))
     }
@@ -19,8 +25,9 @@ const Search = () => {
     return (
         <Consumer>
             {value => {
+                const { dispatch } = value;
                 return (
-                    <form onSubmit={findTrack}>
+                    <form onSubmit={(e) => findTrack(dispatch, e)}>
                         <input type="text"
                             name="trackTitle"
                             id="trackTitle" value={trackTitle}
